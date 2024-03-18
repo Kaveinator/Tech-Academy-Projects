3aff35b97fbc18f14f024e85e485864be1177e20
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-03-18 11:46:42 -0700

    Create ConstosoCrafts Demo

---
+++ 7VisualStudioAndDotNET/ContosoCrafts/ContosoCrafts.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace ContosoCrafts.Website.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
