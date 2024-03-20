69a5965c663c2acfe354e3d5f3d0df5c9ee848c2
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2024-03-20 09:02:17 -0700

    Daily Report Assignment (C# Step 164)

--- 8CSharpAndDotNET/MyConsoleProject/Program.cs
+++ 8CSharpAndDotNET/MyConsoleProject/Program.cs
@@ -7,10 +7,44 @@ using System.Threading.Tasks;
 namespace MyConsoleProject {
     internal class Program {
         static void Main() {
-            Console.Write("What is your name? ");
+            /*Console.Write("What is your name? ");
             string name = Console.ReadLine();
             Console.WriteLine($"Hello, {name}!");
-            Console.WriteLine("Welcome to Acme Accounting Systems\nRememeber, we're \"accounting\" on you!");
+            Console.WriteLine("Welcome to Acme Accounting Systems\nRememeber, we're \"accounting\" on you!");*/
+            
+            // Req 164.1, 164.2, 164.4a
+            Console.Write("The Tech Academy\nStudent Daily Report\nWhat's your name? ");
+            string studentName = Console.ReadLine();
+            // Req 164.4b
+            Console.Write("What course are you on? ");
+            string courseName = Console.ReadLine();
+            // Req 164.4c
+            string tempStr = null; // A temp variable used for parsing
+            while (tempStr == null || !ushort.TryParse(tempStr, out ushort stepNum)) {
+                Console.Write("What step are you on? ");
+                tempStr = Console.ReadLine();
+            }
+            // Req 164.4d
+            tempStr = null; // Not really required here, but resetting parse variable
+            while (tempStr == null || !bool.TryParse(tempStr, out bool needsHelp)) {
+                Console.Write("Do you need help? (true/false) ");
+                tempStr = Console.ReadLine();
+            }
+            // Req 164.4e
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics. ");
+            string positiveExperiences = Console.ReadLine();
+            // Req 164.4f
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific. ");
+            string feedback = Console.ReadLine();
+            // Req 164.4g
+            tempStr = null; // Reset parse variable
+            while (tempStr == null || !byte.TryParse(tempStr, out byte hoursStudied)) {
+                Console.Write("How many hours did you study? ");
+                tempStr = Console.ReadLine();
+            }
+
+            // Req 164.5
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.Read();
         }
     }
